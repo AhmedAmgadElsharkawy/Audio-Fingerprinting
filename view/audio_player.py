a1fc8306e32c916c3e1e14e660c75a2e0f66e8fa
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-12-28 16:46:10 +0200

    playing the song

--- view/audio_player.py
+++ view/audio_player.py
@@ -1,12 +1,15 @@
 from PyQt5.QtWidgets import QPushButton,QWidget,QVBoxLayout,QHBoxLayout,QLabel
 from PyQt5.QtGui import QIcon
 import pyqtgraph as pg
-from PyQt5.QtCore import QSize,Qt
+from PyQt5.QtCore import QSize, Qt, QUrl
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+
 
 class AudioPlayer(QWidget):
     def __init__(self,header):
         super().__init__()
         self.playing = False
+        self.filepath = None
         self.central_layout = QVBoxLayout(self)
         self.central_layout.setContentsMargins(0,0,0,0)
         self.main_widget = QWidget()
@@ -32,6 +35,8 @@ class AudioPlayer(QWidget):
         self.controls_widget_layout.addWidget(self.play_and_pause_button)
 
         self.play_and_pause_button.setCursor(Qt.CursorShape.PointingHandCursor)
+
+        self.media_player = QMediaPlayer()
         
         self.setStyleSheet("""
         #viewer_main_widget{
@@ -42,12 +47,16 @@ class AudioPlayer(QWidget):
         
 
     def toggle_playing(self):
+        if not self.filepath:
+            return
         self.playing = not self.playing
         if self.playing:
             self.play_and_pause_button.setText("Pause")
+            self.media_player.play()
             # self.play_and_pause_button.setIcon(self.pause_icon)
         else:
             self.play_and_pause_button.setText("Play")
+            self.media_player.pause()
             # self.play_and_pause_button.setIcon(self.play_icon)
 
 
--- view/input_player.py
+++ view/input_player.py
@@ -1,7 +1,8 @@
 from PyQt5.QtWidgets import QFileDialog,QPushButton,QLabel
 from view.audio_player import AudioPlayer
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QUrl
 import os
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 
 class InputPlayer(AudioPlayer):
     def __init__(self,audio_signal,header):
@@ -14,6 +15,8 @@ class InputPlayer(AudioPlayer):
 
     def uploadSignal(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Open .wav", "", "*.wav")
+        self.filepath = file_path
+        self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(self.filepath)))
         file_name = os.path.basename(file_path)
         self.header_label.setText(file_name)
         if file_path:
