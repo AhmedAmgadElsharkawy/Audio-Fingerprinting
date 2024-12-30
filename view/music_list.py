5eb8bb4d1374940cc1e84d83cad415b4554ef5cd
Mohamed-185 <ma4910656@gmail.com>
2024-12-30 21:44:11 +0200

    Adding a default sorting

--- view/music_list.py
+++ view/music_list.py
@@ -1,14 +1,15 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QScrollArea,QGridLayout
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QScrollArea
 from PyQt5.QtCore import Qt
-
 from view.music_card import MusicCard
+
 class MusicList(QWidget):
-    def __init__(self,songs_list):
+    def __init__(self, songs_list):
         super().__init__()
         self.setFixedWidth(500)
         self.songs_list = songs_list
         self.central_layout = QVBoxLayout(self)
         self.central_layout.setContentsMargins(0,0,0,0)
+        
         self.main_widget = QWidget()
         self.main_widget.setObjectName("list_viewer_main_widget")
         self.main_widget_layout = QVBoxLayout(self.main_widget)
@@ -22,13 +23,25 @@ class MusicList(QWidget):
 
         self.central_layout.addWidget(self.scroll_area)
 
-        self.load_music_cards()
+        # Default song data
+        self.default_songs = [
+            {"title": "Save Your Tears", "artist": "Group1", "image": "data/images/1.jpg"},
+            {"title": "Let Her Go", "artist": "Group13", "image": "data/images/13.jpg"},
+            {"title": "Please", "artist": "Group9", "image": "data/images/9.jpg"},
+            {"title": "NinaCriedPower", "artist": "Group5", "image": "data/images/5.jpg"},
+            {"title": "Ya Lala", "artist": "Group15", "image": "data/images/15.jpg"},
+            {"title": "Wen Elkhael", "artist": "Group10", "image": "data/images/10.jpg"},
+            {"title": "SomeLikeYou", "artist": "Group3", "image": "data/images/3.jpg"},
+            {"title": "A Thousand Years", "artist": "Group8", "image": "data/images/8.jpg"},
+            {"title": "FEIN", "artist": "Group11", "image": "data/images/11.jpg"},
+            {"title": "A Sky Full of Stars", "artist": "Group14", "image": "data/images/14.jpg"}
+        ]
 
-    
+        self.load_default_songs()
+
         self.setStyleSheet("""
-
             QScrollArea {
-                border:none;
+                border: none;
             }
             QScrollBar:vertical {
                 border: none;
@@ -36,7 +49,7 @@ class MusicList(QWidget):
                 width: 7px;
                 margin: 0px;
                 border-radius: 5px;
-                border-radius:3px;
+                border-radius: 3px;
             }
             QScrollBar::handle:vertical {
                 background: #047cdc;
@@ -56,11 +69,34 @@ class MusicList(QWidget):
             }
         """)
 
-
-    def load_music_cards(self):
-        for i,song in enumerate(self.songs_list):
-            music_card = MusicCard(song_order=str(i+1),artist_name="The Weeknd",album_cover_path="data/music/song1/song_img.jpeg",song_title="save your tears",similarity_ratio_value = 50)
-            self.main_widget_layout.addWidget(music_card)
+    def load_default_songs(self):
+        # Clear existing cards
+        for i in reversed(range(self.main_widget_layout.count())): 
+            self.main_widget_layout.itemAt(i).widget().setParent(None)
             
+        # Add default songs
+        for i, song in enumerate(self.default_songs):
+            music_card = MusicCard(
+                song_order=str(i + 1),
+                album_cover_path=f"data/images/{song['artist']}.jpg",
+                song_title=song["title"],
+                artist_name=song["artist"],
+                similarity_ratio_value=0  # Start with 0%
+            )
+            self.main_widget_layout.addWidget(music_card)
 
-        
+    def update_songs(self, similarities):
+        # Clear existing cards
+        for i in reversed(range(self.main_widget_layout.count())): 
+            self.main_widget_layout.itemAt(i).widget().setParent(None)
+            
+        # Add new cards based on similarities
+        for i, song in enumerate(similarities[:10]):  # Limit to 10 songs
+            music_card = MusicCard(
+                song_order=str(i + 1),
+                album_cover_path=f"data/images/{song['artist']}.jpg",
+                song_title=song['title'],
+                artist_name=song['artist'],
+                similarity_ratio_value=int(song['similarity'])
+            )
+            self.main_widget_layout.addWidget(music_card)
